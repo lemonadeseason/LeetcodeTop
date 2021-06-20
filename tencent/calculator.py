2937ece8117478f42b2f9923fe4b303bf0cd5650
lemonadeseason <31380516+lemonadeseason@users.noreply.github.com>
2021-06-20 23:28:18 +0800

    Update calculator.py
    以固定pattern处理四则运算，之前有歪打正着的bug，fix掉了

--- tencent/calculator.py
+++ tencent/calculator.py
@@ -1,3 +1,8 @@
+//input case；s = "3+2*2"
+//input case；s = " 3/2 "
+//input case；s = " 3+5 / 2 "
+//while循环里捕捉到了input的重复模式；  空格若干  数字  空格若干（歪打正着switch不对应四则运算）  符号   每一次while就处理这样的一个模式  “   5   +”,唯一特例是最后“   5  ”，不会再读符号了
+
 class Solution {
 public:
     int calculate(string s) {
@@ -15,13 +20,15 @@ public:
             num = num*10 + int(s[i]-'0');
             i++;
         }
+        while(i<len&&s[i]==' ') i++;
         //if(i==len) {break;}
-        switch(pre_op)
+        switch(pre_op)    //当到最后末尾时，无论是数字还是空格都会进入这里
         {
             case '+':val_stack.push_back(num);break;
             case '-':val_stack.push_back(-num);break;
             case '*':{val_stack.back()*=num;break;}
             case '/':{val_stack.back()/=num;break;}
+            //default:return 1000;
         }
         if(i==len) break;   
         pre_op = s[i];
